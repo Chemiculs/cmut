4aacabb9b0dc508854e316d1491267f9835ee395
lane graham <lanegraham3@gmail.com>
2025-07-13 21:14:30 -0700

    Add project files.

---
+++ entrypoint.cxx
@@ -0,0 +1,17 @@
+#include "cmut.hxx"
+#include <iostream>
+
+int main() noexcept {
+
+	cmut<std::uint64_t> mut(4096 * 4096);
+
+	mut /= 4096;
+
+	mut += 8;
+
+	mut -= 4;
+	
+	std::cout << mut.get() << std::endl;
+
+	return 0;
+}
